c03cb62bf383c97012d48aa2bc8becf44070d648
yaohp <yaohp@kamfu.com.cn>
2024-05-21 17:06:57 +0800

    fix:sqlite 查询前建个表？

--- server/api/db/query.ts
+++ server/api/db/query.ts
@@ -1,5 +1,6 @@
 export default defineEventHandler(async () => {
   const db = useDatabase()
+  await db.sql`CREATE TABLE IF NOT EXISTS plv_pv ("id" TEXT PRIMARY KEY, "msg" TEXT, "ip" TEXT, "date" TEXT, "addr" TEXT)`;
   // 查询数据
   const { rows } = await db.sql`SELECT * FROM plv_pv order by date desc`;
 
